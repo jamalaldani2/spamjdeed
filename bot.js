c7bb2b8a7c7651b411642eff3a6eb0873cb49d76
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2019-05-01 10:19:38 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,5 +57,5 @@ client.on('message', message => {
 });
 
 client.login(process.env.BOT_TOKEN);
-client.login(process.env.BOT_TOKEN2);
+bot.login(process.env.BOT_TOKEN2);
 client.login(process.env.BOT_TOKEN3);
