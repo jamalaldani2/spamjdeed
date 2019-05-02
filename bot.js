69fd504294dc2168be5534b5be0e3e7d3048dc58
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2019-05-02 01:48:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,15 +2,11 @@ console.log("BOT ONLINE");
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const bot = new Discord.Client();
-
-client.on('ready', () => {//Toxic Codesa
-    console.log(`Logged in as ${client.user.tag} !`);//Toxic Codes
-          client.user.setActivity("by n3k4a|Premiumbot|Toxic Codes  .",{type: 'WATCHING'});//Toxic Codes
   //Toxic Codes
   });//Toxic Codes
 
 bot.on('ready', () => {//Toxic Codes
-    console.log(`Logged in as ${client.user.tag} !`);//Toxic Codes
+    console.log(` !`);//Toxic Codes
           bot.user.setActivity("by n3k4a|Premiumbot|Toxic Codes  .",{type: 'WATCHING'});//Toxic Codes
   //Toxic Codes
   });//Toxic Codes
@@ -21,10 +17,7 @@ client.on('ready', () => {//Toxic Codes
   //Toxic Codes
   });//Toxic Codes
 
-client.on('ready', () => {//Toxic Codes
-    console.log(`Logged in as ${client.user.tag} !`);//Toxic Codes
-          client.user.setActivity("by n3k4a|Premiumbot|Toxic Codes  .",{type: 'WATCHING'});//Toxic Codes
-  //Toxic Codes
+
   });//Toxic Codes
 
 
